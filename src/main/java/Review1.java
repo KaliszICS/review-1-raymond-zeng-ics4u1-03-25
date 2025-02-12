af1bfe275312aa82bf78b24b1f686d7971d0ec85
raymond-zeng-ics4u1-03-25 <raymond.zeng4@student.tdsb.on.ca>
2025-02-12 19:26:39 +0000

    update files

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -1,23 +1,38 @@
 public class Review1 {
 
+	public static void main() {
+
+		q1();
+		q2();
+		q3();
+		q4();
+		q5();
+	}
 	public static void q1() {
-		//Write question 1 code here
+		System.out.println("There once was a man from St. Ives.");
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		int x = 9;
+		int y = 5;
+		System.out.println(x / y);
+		System.out.println(x % y);
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		System.out.println("Hello\nHello again");
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		boolean isNum = true;
+		System.out.println(isNum);
 	}
 
 	public static void q5() {
-		//Write question 5 code here
+		String word1 = "Hello";
+		String word2 = "World";
+		System.out.print(word1 + " ");
+		System.out.println(word2);
 	}
 
 }
